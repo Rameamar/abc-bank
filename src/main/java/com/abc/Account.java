bb08b6e65a1fca84ab4c121a6dc877fc2e3cea39
Rameamar <ram.amaraneni@gmail.com>
2016-08-04 17:23:25 -0400

    Update Account.java

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -45,11 +45,12 @@ public void withdraw(double amount) {
 //                if (amount <= 4000)
 //                    return 20;
             case MAXI_SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.02;
                 if (amount <= 2000)
-                    return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
+                    return amount * 0.02;
+                if (amount <= 3000)
+                    return 20 + (amount-2000) * 0.05;
+                if (amount>3000)
+                return 70 + (amount-3000) * 0.1;
             default:
                 return amount * 0.001;
         }
